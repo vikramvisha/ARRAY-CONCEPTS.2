995dec334e136ef25464967a44ea87f0f978e86c
Vikram G <130371116+vikramvisha@users.noreply.github.com>
2024-08-05 17:25:38 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+// 6.inserting a element inarray
+let arra1=[5,23.54,54,12,22,56]
+arra1.push(36,32)
+console.log(arra1)
+arra1.pop()
+console.log(arra1)
+ arra1.shift(2)
+ console.log(arra1)
+arr1.unshift(21)
+console.log(arra1)
+console.log(arra1.indexOf(12))
+let newarra=arra1.map(x=> x*2)
+console.log(newarra)
+console.log(newarra.reverse())
+let arr3=arra1.sort();
+console.log(arr2)
+let a=[1,2,3,4.6,5,7]
+let c=a.includes(5)
+console.log(c)
+
+ //7.sum of array
+ let i;
+ let sum=0
+ for(i=0;i<arr1.length;i++)
+ {
+    sum=sum+arr1[i]
+
+ }
+// 8.converting array to string
+
+for(i=0;i<arr1.length;i++)
+{
+    console.log("Array Elements :",arr1[i])
+
+}
